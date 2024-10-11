994918defa780a50c75fa1c303966266c5d0d6c4
bebel <129115606+isabelconc@users.noreply.github.com>
2024-10-11 06:07:36 -0300

    Delete rep directory

--- rep/controllers/controllers.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-from models.pessoa import pessoa
-
-app2 = Blueprint("exemplo", __name__)
-
-@app2.route("/")
-def exibir():
-    return render_template('index.html')
-
